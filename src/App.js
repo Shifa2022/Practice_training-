c691806f9b05894c2a26ded3caa3f05883868a8c
unknown <raklinux46@iiht.tech>
2022-02-22 23:50:32 +0530

    new changes badeg

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import Card from './Card';
 import Message from './Message';
+import Badge from './Badge';
+
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Message/>
       <Card title="REACT"/>
       <Card title="NODEJS"/>
+      <Badge caption="Inbox" count="10"/>
+    <Badge caption="Sent" count="1"/>
     </div>
        
         
